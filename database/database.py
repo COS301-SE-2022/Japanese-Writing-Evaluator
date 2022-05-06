7ced8861a1cac72e0456d5d58bef476fe2c6400f
Siphesihle05 <u19078286@tuks.co.za>
2022-05-07 01:56:28 +0200

    Created a database function that get a user by email for Forgot password

---
+++ database/database.py
@@ -0,0 +1,31 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+class Database:
+    """
+        Constructor:
+            create a connect to the database
+            returns None if connection fails
+    """
+    def __init__(self):
+        try:
+            self.conn = psycopg2.connect(host = os.getenv('DB_HOST'), database = os.getenv('DB_NAME'), user = os.getenv('DB_USER'), password = os.getenv('DB_PASS'))
+            self.curr = self.conn.cursor()
+            print("connected")
+        except print(0):
+            print("Could not connect to database")
+            return None
+    
+    def findAll(self):
+        query = "SELECT * FROM users;"
+        self.curr.execute(query,)
+        users = self.curr.fetchall()
+        return users
+
+    def getUserByEmail(self, email):
+        query = " SELECT username FROM users WHERE email = %s"
+        self.curr.execute(query, (email,))
+        name = self.curr.fetchone()
+        return name
