57f417c849bb0086913ca24d4fc9927b70a608ec
Siphesihle05 <u19078286@tuks.co.za>
2022-05-11 22:06:37 +0200

    Added jwt token for authentication

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify, request
+from functools import wraps
+from flask import Flask, jsonify, request, session
+from datetime import datetime, timedelta
+import jwt
 import hashlib
 import uuid
 
@@ -8,7 +11,27 @@ sys.path.append('../database')
 from database import Database
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='459758192b5ba092efb54f9094237481'
 db = Database()
+
+def token_required(function):
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        token = None
+        print(request.headers)
+        if 'user-token' in request.headers:
+            print("we have token")
+            token = request.headers['user-token']
+        if not token:
+            return jsonify({'arlet' : 'Token is missing !!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return jsonify({'arlet' : 'The token is invaild!'}), 401
+        return  function(*args, **kwargs)
+  
+    return decorated 
+
 
 @app.route('/')
 def lancher():
@@ -72,6 +95,7 @@ def register():
 """
 
 @app.route('/upload', methods = ['POST'])
+@token_required
 def uplaodImage():
     succ = db.saveImage(int(request.json["id"]), str(request.json["image_path"]), str(request.json["image_char"]), int(request.json["score"]))
     if succ:
@@ -89,8 +113,9 @@ def uplaodImage():
 """
 
 @app.route('/view', methods = ['GET'])
+@token_required
 def viewImages():
-    images = db.getImage(str(request.json["id"]))
+    images = db.getImage(int(request.json["id"]))
     if images:
         return jsonify({'response': images}), 200
     else:
@@ -107,6 +132,12 @@ def login():
     if user == None: 
         return jsonify({'response': "user not found."}), 401
     else: 
-        return jsonify({'response': user}), 200
+        session["logged_in"] = True
+        token = jwt.encode({
+            'username' : user[0],
+            'id': user[1],
+            'experation': str(datetime.utcnow() + timedelta(seconds=120)),
+        }, app.config['SECRET_KEY'], "HS256")
+        return jsonify({'response': 'user login succesful', 'user-token':token, 'data': user}), 200
 if __name__ == '__main__':
     app.run(debug = True)
