a15752311db9eb7048534b55fc3726f56989a373
Maryam-Al-Mahdi <u19009977@tuks.co.za>
2022-05-11 19:20:28 +0200

    Add a signup class

---
+++ src/app/sign-up/sign-up.ts
@@ -0,0 +1,13 @@
+export class SignUpPage
+{
+  username: string;
+  email: string;
+  password: string;
+
+  constructor(username, email, password)
+  {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
