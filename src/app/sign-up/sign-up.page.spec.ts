122534bb6e8ac7161816cc7cbecc3b6a5bd7f14c
Maryam-Al-Mahdi <u19009977@tuks.co.za>
2022-05-11 16:30:09 +0200

    signup form integrated

--- src/app/sign-up/sign-up.page.spec.ts
+++ src/app/sign-up/sign-up.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
 
@@ -11,7 +12,7 @@ describe('SignUpPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SignUpPage ],
-      imports: [IonicModule.forRoot(),RouterTestingModule]
+      imports: [IonicModule.forRoot(),RouterTestingModule, FormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignUpPage);
