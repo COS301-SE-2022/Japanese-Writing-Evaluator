5e18739bdd18f377a9a93f01b043fb6e5b54d4bc
Raymond0413 <sechaba836@gmail.com>
2022-05-12 13:53:48 +0200

    add linting

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ref, getDownloadURL, listAll, getStorage } from 'firebase/storage';
 // import { AppServiceService } from '../services/app-service.service';
 // import { Character } from '../shared/character';
-import { storage, app } from 'Storage/firebaseConfig'
+import { storage, app } from 'Storage/firebaseConfig';
 
 @Component({
   selector: 'app-home',
@@ -18,54 +18,49 @@ export class HomePage implements OnInit {
     //   this.characters = res;
     // });
 
-    suggestCharacter();
+    this.suggestCharacter();
     // this.repository.getProgress();
       // var images = suggestCharacter();
       // var suggestion1 = document.getElementById("Suggestion1");
-      // suggestion1.innerHTML = 
+      // suggestion1.innerHTML =
       // '<img alt="Suggestion" src="'+images[0]+'"/>'
       // + '<ion-button  color="dark" id="try">Try</ion-button>';
 
       // var suggestion2 = document.getElementById("Suggestion2");
-      // suggestion2.innerHTML = 
+      // suggestion2.innerHTML =
       // '<img alt="Suggestion" src="'+images[1]+'"/>'
       // + '<ion-button  color="dark" id="try">Try</ion-button>';
   }
 
-  // uploadImage(){
-  //   //when uploadimage button is click
-  //   this.repository.uploadImage();
-  // }
-
-}
-
-function suggestCharacter()
+  suggestCharacter()
   {
-    let images = [];
-    const folderRef = ref(storage, "characters/Hiragana")
+    //eslint-disable-nextline prefer-const
+    const images: string[] = [];
+    //eslint-disable-nextline prefer-const
+    const suggested: string[] = [];
+    const folderRef = ref(storage, 'characters/Hiragana');
 
     listAll(folderRef).then((response) => {
-      console.log(response)
       response.items.forEach((pictures) => {
         getDownloadURL(pictures).then((urls) => {
-          images.push(urls);
-          if(response.items.length == images.length)
+          images.push(urls.toString());
+          if(response.items.length === images.length)
           {
-            var random = 0;
-            var suggested = [];
-            var chosen = 0;
-            for(var i = 0; i < 2; i++)
+
+            let random = 0;
+            let chosen = 0;
+            for(let i = 0; i < 2; i++)
             {
               random = Math.floor(Math.random() * images.length);
-              if(i == 0)
+              if(i === 0)
               {
                 suggested.push(images[random]);
               }
               else
               {
-                if(chosen == random)
+                if(chosen === random)
                 {
-                  while(chosen == random)
+                  while(chosen === random)
                   {
                     random = Math.floor(Math.random() * images.length);
                   }
@@ -77,19 +72,23 @@ function suggestCharacter()
                 }
               }
 
-              chosen = random
+              chosen = random;
             }
 
             console.log(suggested);
             console.log(suggested.length);
-            var img1 = document.getElementById("suggest1");
-            img1.setAttribute("src", suggested[0]);
-            var img2 = document.getElementById("suggest2");
-            img2.setAttribute("src", suggested[1]);
-          
+            const img1 = document.getElementById('suggest1');
+            img1.setAttribute('src', suggested[0]);
+            const img2 = document.getElementById('suggest2');
+            img2.setAttribute('src', suggested[1]);
           }
-        })
-      })
-    })
-    
+        });
+      });
+    });
   }
+  // uploadImage(){
+  //   //when uploadimage button is click
+  //   this.repository.uploadImage();
+  // }
+
+}
