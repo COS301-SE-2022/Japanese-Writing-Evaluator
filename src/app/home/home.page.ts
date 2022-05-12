6eda8d306f6cc2994af6efe5768583e0bbbd630b
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-05-12 16:20:30 +0200

    ⚡️ frontend -feature - uploadImage => update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 // import { AppServiceService } from '../services/app-service.service';
 // import { Character } from '../shared/character';
-import { storage , app } from 'Storage/firebaseConfig';
+import { storage, app } from 'Storage/firebaseConfig';
+import { AppServiceService } from '../services/app-service.service';
+import firebase from 'firebase/compat';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  // characters: Character[] = [];
-  constructor() {}//private repository: AppServiceService
+  image: File = null;
+  upload: FormGroup;
+  constructor(formBuilder: FormBuilder, private repository: AppServiceService) {
+    this.upload = formBuilder.group({ // building a responsive form with two inputs
+      image: new FormControl('',[Validators.required]),
+    });
+  }//private repository: AppServiceService
 
   ngOnInit(): void {
     // this.repository.getCharacters().subscribe(res => {
@@ -19,8 +28,19 @@ export class HomePage implements OnInit {
     // this.repository.getProgress();
   }
 
-  // uploadImage(){
-  //   //when uploadimage button is click
-  //   this.repository.uploadImage();
-  // }
+  getImage(event){
+    this.image = event.target.files[0];
+  }
+
+  uploadImage(){
+    //const storage = getStorage();
+    console.log(this.upload.controls.image.value.split('\\').length-1);
+    const imageurl= this.upload.controls.image.value.split('\\');//
+    const imageRef = ref(storage,`users/${imageurl[imageurl.length-1]}`);
+    const uploadTask = uploadBytesResumable(imageRef, this.image);
+    uploadTask.on('state_changed',(err) =>{
+      console.log(err);
+    });
+  //  this.repository.uploadImage(this.upload.controls.image.value);
+  }
 }
