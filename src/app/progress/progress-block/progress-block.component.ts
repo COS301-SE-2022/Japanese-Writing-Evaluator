e787dbb1e18747df32a46c493027fd1652200245
Maryam-Al-Mahdi <u19009977@tuks.co.za>
2022-06-06 01:12:17 +0200

    ✅ dynamic percent

--- src/app/progress/progress-block/progress-block.component.ts
+++ src/app/progress/progress-block/progress-block.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Character } from './../../shared/character';
+import { Component, Input, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-block',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-block.component.scss'],
 })
 export class ProgressBlockComponent implements OnInit {
+  @Input() percent: number;
+  @Input() letter: string;
 
-  constructor() { }
+  char: string;
+  divStyle: string;
+  styles: any;
+
+
+
+  constructor() {
+   }
 
   ngOnInit() {}
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    this.char = this.letter;
+  }
+
+  // document.getElementById('percentFill').style.strokeDashoffset = 'calc(440 - (440 *' + this.percent +') / 100)';
+
+  setStyleCalc()
+  {
+    this.styles = {'stroke-dashoffset': 'calc(440 - (440 *' + this.percent +') / 100)'};
+    return this.styles;
+  }
 
 }
