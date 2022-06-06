e435f598b5f1c54f2e51802beda5744cdffbfb79
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-06-06 17:18:14 +0200

    Update createdataset.py

--- backend/createdataset.py
+++ backend/createdataset.py
@@ -15,73 +15,47 @@ class createTrainImages:
     index_U = 0 
     pathA = os.getcwd() + '/datasetA'
     pathU = os.getcwd() + '/datasetU'
+    
 
-    def freq_A_images(train_labels): 
-
-    #get the freq of each letter 
-    #create an array that holds the index where the letter is
+    def  create_A_images(self,train_imgs): 
         for i in train_labels:
             if train_labels[i] == 0:
-                freq_a=freq_a+1
+                self.freq_a=self.freq_a+1
     
-        a_index =[] * freq_a 
+        a_index =[] * self.freq_a 
         for i in train_labels:
             if train_labels[i] == 0:
                 a_index.append(i)
-
- 
-
-    def  create_A_images(train_imgs): 
-        for n in range (0,2):
-            index_A = Dataset.a_index[n]
+        index =0 
+        for n in range (0,self.freq_a):
+            index= a_index[n]
             filename = "image"
             number = str(n)
             extention = ".jpeg"
-            cv2.imwrite(os.path.join(Dataset.pathA ,filename+number+extention),train_imgs[index_A])
+            cv2.imwrite(os.path.join(Dataset.pathA ,filename+number+extention),train_imgs[index])
 
 
 
-    def create_U_images(train_imgs):
-        for n in range (0,2): 
-            index_U = Dataset.u_index[n]
-            filename = "image"
-            number = str(n)
-            extention = ".jpeg"
-            cv2.imwrite(os.path.join(Dataset.pathU ,filename+number+extention),train_imgs[index_U])
-
-    def freq_U_images(train_labels): 
-        freq_u = 0 
-        u_index = [] 
+    def create_U_images(self,train_imgs):
         for i in train_labels:
-            if train_labels[i] == 0:
-                freq_u=freq_u+1
+            if train_labels[i] == 2:
+                self.freq_u=self.freq_u+1
     
-        u_index =[] * freq_u 
+        u_index =[] * self.freq_u 
         for i in train_labels:
-            if train_labels[i] == 0:
+            if train_labels[i] == 2:
                 u_index.append(i)
-
-def Main(): 
-    # Paths
-    input_path = os.path.join('.', 'input')
-
-    # loading npz file to become np array 
-    k49_train_imgs_path = os.path.join(input_path, 'k49-train-imgs.npz')
-    k49_train_labels_path = os.path.join(input_path, 'k49-train-labels.npz')
-    k49_test_imgs_path = os.path.join(input_path, 'k49-test-imgs.npz')
-    k49_test_labels_path = os.path.join(input_path, 'k49-test-labels.npz')
-
-    #making np array
-    train_imgs = np.load(k49_train_imgs_path)['arr_0']
-    train_labels = np.load(k49_train_labels_path)['arr_0']
-
-
-    
-    #create_A_images()
+        index =0 
+        for n in range (0,self.freq_u):
+            index= u_index[n]
+            filename = "image"
+            number = str(n)
+            extention = ".jpeg"
+            cv2.imwrite(os.path.join(Dataset.pathU ,filename+number+extention),train_imgs[index])
 
 if __name__ == '__main__':
-        # Paths
-    Dataset = createTrainImages()
+    # Paths
+    
     input_path = os.path.join('.', 'input')
 
     # loading npz file to become np array 
@@ -93,8 +67,21 @@ if __name__ == '__main__':
     #making np array
     train_imgs = np.load(k49_train_imgs_path)['arr_0']
     train_labels = np.load(k49_train_labels_path)['arr_0']
-
+    #create a dataset object
+    Dataset = createTrainImages()
+    #create A training images 
     cv2.imread(Dataset.pathA)
-    os.mkdir(Dataset.pathA)
+    if os.path.exists(Dataset.pathA):
+       pass
+    else:
+        os.mkdir(Dataset.pathA)
+    
+    Dataset.create_A_images(train_imgs)
+    #create U training images 
     cv2.imread(Dataset.pathU)
-    os.mkdir(Dataset.pathU)
+    if os.path.exists(Dataset.pathA):
+           pass
+    else:
+        os.mkdir(Dataset.pathA)
+    
+    Dataset.create_U_images(train_imgs) 
