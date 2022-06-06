716544c10f0929ba852898d720244efa11ce869d
Maryam-Al-Mahdi <u19009977@tuks.co.za>
2022-06-06 15:58:21 +0200

    ✅ Added block for trying characters

---
+++ src/app/home/block-try-char/block-try-char.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-try-char',
+  templateUrl: './block-try-char.component.html',
+  styleUrls: ['./block-try-char.component.scss'],
+})
+export class BlockTryCharComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
