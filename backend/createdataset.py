7655ade55e64ccbe82ec8c9efb39dc53d916d49c
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-06-06 15:56:29 +0200

    Update createdataset.py

--- backend/createdataset.py
+++ backend/createdataset.py
@@ -8,19 +8,23 @@ from PIL import Image
 import cv2
 
 
-def createdataset():
+
+def Main(): 
     # Paths
     input_path = os.path.join('.', 'input')
     classmap_path = os.path.join(input_path, 'k49_classmap.csv')
 
+    # loading npz file to become np array 
     k49_train_imgs_path = os.path.join(input_path, 'k49-train-imgs.npz')
     k49_train_labels_path = os.path.join(input_path, 'k49-train-labels.npz')
     k49_test_imgs_path = os.path.join(input_path, 'k49-test-imgs.npz')
     k49_test_labels_path = os.path.join(input_path, 'k49-test-labels.npz')
 
-    # Learning
-    n_classes = 49
-    learning_rate = 0.001
-    image_shape = (28, 28, 1)
-    n_epochs = 25
-
+    #making np array
+    train_imgs = np.load(k49_train_imgs_path)['arr_0']
+    train_labels = np.load(k49_train_labels_path)['arr_0']
+    test_imgs = np.load(k49_test_imgs_path)['arr_0']
+    test_labels = np.load(k49_test_labels_path)['arr_0']
+ 
+    #create_A_images()
+    #create_A_images(train_labels)
