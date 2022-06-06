7b36c8145f93d4d7aad5974c6bbf45c5711a961a
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-06-06 16:16:41 +0200

    Update createdataset.py

--- backend/createdataset.py
+++ backend/createdataset.py
@@ -6,11 +6,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+from torch import index_add
 
 freq_a = 0
 a_index =[]
 freq_u = 0 
 u_index = []
+index_A = 0
+index_U = 0 
+pathA = ''
+pathU = ''
+
 def create_A_images(train_labels): 
 
     #get the freq of each letter 
@@ -24,22 +30,10 @@ def create_A_images(train_labels):
         if train_labels[i] == 0:
             a_index.append(i)
 
-def create_U_images(train_labels): 
-    freq_u = 0 
-    u_index = [] 
-    for i in train_labels:
-        if train_labels[i] == 0:
-            freq_u=freq_u+1
-    
-    u_index =[] * freq_u 
-    for i in train_labels:
-        if train_labels[i] == 0:
-            u_index.append(i)
 
 def Main(): 
     # Paths
     input_path = os.path.join('.', 'input')
-    classmap_path = os.path.join(input_path, 'k49_classmap.csv')
 
     # loading npz file to become np array 
     k49_train_imgs_path = os.path.join(input_path, 'k49-train-imgs.npz')
@@ -50,8 +44,11 @@ def Main():
     #making np array
     train_imgs = np.load(k49_train_imgs_path)['arr_0']
     train_labels = np.load(k49_train_labels_path)['arr_0']
-    test_imgs = np.load(k49_test_imgs_path)['arr_0']
-    test_labels = np.load(k49_test_labels_path)['arr_0']
- 
+
+    
+    cv2.imread(pathA)
+    os.mkdir(pathA)
+    cv2.imread(pathU)
+    os.mkdir(pathU)
     #create_A_images()
-    #create_A_images(train_labels)
+
