2a875724cf8e97dc1d4f73b6d2d32e2d50e47235
Phumudzo V. Ndou <49733146+Vhulenda88@users.noreply.github.com>
2022-06-09 10:10:52 +0200

    Update block-try-char.component.spec.ts

--- src/app/home/block-try-char/block-try-char.component.spec.ts
+++ src/app/home/block-try-char/block-try-char.component.spec.ts
@@ -2,8 +2,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-import { RouterTestingModule } from '@angular/router/testing';
-
 import { BlockTryCharComponent } from './block-try-char.component';
 
 describe('BlockTryCharComponent', () => {
