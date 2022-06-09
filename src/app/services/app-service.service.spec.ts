9118d94881e2290d5f729689482f5fd680d08915
Vhulenda88 <u19050993@tuks.co.za>
2022-06-10 00:39:07 +0200

    added test for upload

--- src/app/services/app-service.service.spec.ts
+++ src/app/services/app-service.service.spec.ts
@@ -1,17 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { AppServiceService } from './app-service.service';
+import { UploadedImage } from '../shared/image';
 
 describe('AppServiceService', () => {
   let service: AppServiceService;
-
+  let mockhttpRequest: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [AppServiceService],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule],//, HttpTestingController
     });
     service = TestBed.inject(AppServiceService);
+    mockhttpRequest = TestBed.inject(HttpTestingController);
   });
+
+  // it('check for invalid image', inject(
+  //   [HttpTestingController, AppServiceService],
+  //   () => {
+  //     const mockScore = 0;
+  //     let mockimage = new Object() as UploadedImage;
+  //     mockimage = {
+  //       id: '82',
+  //       image: '',
+  //       imagechar: 'U',
+  //       file: 'u.png',
+  //     };
+  //     service.uploadImage(mockimage).subscribe(data => {
+  //       expect(data).toEqual(mockScore);
+  //     });
+
+  //     const mockuploadrequest = mockhttpRequest.expectOne(service.baseURL+'/upload');
+  //     expect(mockuploadrequest.request.method).toEqual('POST');
+  //     expect(mockuploadrequest.request.body).toEqual(mockimage);
+  //     mockuploadrequest.flush(mockScore);
+  //   })
+  //   );
+
+  //   it('check for valid image', inject(
+  //     [HttpTestingController, AppServiceService],
+  //     () => {
+  //       const mockScore = 0;
+  //       let mockimage = new Object() as UploadedImage;
+  //       mockimage = {
+  //         id: '82',
+  //         image: '',
+  //         imagechar: 'U',
+  //         file: 'u.png',
+  //       };
+
+  //       service.uploadImage(mockimage).subscribe(data => {
+  //         expect(data.body.score).toBeDefined();
+  //       });
+
+  //       const mockuploadrequest = mockhttpRequest.expectOne(service.baseURL+'/upload');
+  //       expect(mockuploadrequest.request.method).toEqual('POST');
+  //       expect(mockuploadrequest.request.body).toEqual(mockimage);
+  //       mockuploadrequest.flush(mockScore);
+  //     })
+  //     );
+
+  //   afterEach(() => {
+  //     mockhttpRequest.verify();
+  //   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
