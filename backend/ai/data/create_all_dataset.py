7d7ca076fe46cc75a655446f316f2217ce0bbb36
Siphesihle05 <u19078286@tuks.co.za>
2022-07-12 22:42:54 +0200

    Update create_all_dataset.py

--- backend/ai/data/create_all_dataset.py
+++ backend/ai/data/create_all_dataset.py
@@ -92,6 +92,8 @@ if __name__ == '__main__':
     character_array = ['a','i', 'u', 'e', 'o','ka','ki','ku','ke','ko','sa','shi','su','se','so','ta','chi','tsu','te','to','na','ni','nu','ne','no','ha','hi','fu','he','ho','ma','mi','mu','me','mo','ya','yu','yo','ra','ri','ru','re','ro','wa','wo','wi' ,'we','n']
     #create a dataset object
     Dataset = createImages()
+    
+    Dataset.create_All_train_images(train_imgs, character_array)
      #create A training images 
 
     Dataset.create_All_test_images(test_imgs,character_array)
