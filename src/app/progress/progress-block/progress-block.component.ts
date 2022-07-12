af501872df1e2f804f384c46108f89ddd96ce433
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-07-12 22:41:40 +0200

    ✨ Fixed progress logic

--- src/app/progress/progress-block/progress-block.component.ts
+++ src/app/progress/progress-block/progress-block.component.ts
@@ -8,15 +8,108 @@ import { Component, Input, OnInit, AfterViewInit, AfterContentInit } from '@angu
 })
 export class ProgressBlockComponent implements OnInit {
   @Input() percent: number;
-  @Input() letter: number;
+  @Input() letter: string;
+  @Input() alphabetType: string;
 
   japaneseLetter: string;
   translation: string;
   divStyle: string;
   styles: any;
 
-  alphabet: string[] = ['あ', 'い', 'う', 'え', 'お'];
+  hiraganaAlphabet = [
+    {character: 'あ', translation:'A'},
+    {character: 'い', translation:'I'},
+    {character: 'う', translation:'U'},
+    {character: 'え', translation:'E'},
+    {character: 'お', translation:'O'},
+    {character: 'か', translation:'Ka'},
+    {character: 'き', translation:'Ki'},
+    {character: 'く', translation:'Ku'},
+    {character: 'け', translation:'Ke'},
+    {character: 'こ', translation:'Ko'},
+    {character: 'さ', translation:'Sa'},
+    {character: 'し', translation:'Si'},
+    {character: 'す', translation:'Su'},
+    {character: 'せ', translation:'Se'},
+    {character: 'そ', translation:'So'},
+    {character: 'た', translation:'Ta'},
+    {character: 'ち', translation:'Ti'},
+    {character: 'つ', translation:'Tu'},
+    {character: 'て', translation:'Te'},
+    {character: 'と', translation:'To'},
+    {character: 'な', translation:'Na'},
+    {character: 'に', translation:'Ni'},
+    {character: 'ぬ', translation:'Nu'},
+    {character: 'ね', translation:'Ne'},
+    {character: 'の', translation:'No'},
+    {character: 'は', translation:'Ha'},
+    {character: 'ひ', translation:'Hi'},
+    {character: 'ふ', translation:'Hu'},
+    {character: 'へ', translation:'He'},
+    {character: 'ほ', translation:'Ho'},
+    {character: 'ま', translation:'Ma'},
+    {character: 'み', translation:'Mi'},
+    {character: 'む', translation:'Mu'},
+    {character: 'め', translation:'Me'},
+    {character: 'も', translation:'Mo'},
+    {character: 'や', translation:'Ya'},
+    {character: 'ゆ', translation:'Yu'},
+    {character: 'よ', translation:'Yo'},
+    {character: 'ら', translation:'Ra'},
+    {character: 'り', translation:'Ri'},
+    {character: 'る', translation:'Ru'},
+    {character: 'れ', translation:'Re'},
+    {character: 'ろ', translation:'Ro'},
+    {character: 'わ', translation:'Wa'},
+    {character: 'ゐ', translation:'Wi'},
+    {character: 'ゑ', translation:'We'},
+    {character: 'を', translation:'Wo'},
+    {character: 'が', translation:'Ga'},
+    {character: 'ぎ', translation:'Gi'},
+    {character: 'ぐ', translation:'Gu'},
+    {character: 'げ', translation:'Ge'},
+    {character: 'ご', translation:'Go'},
+    {character: 'ざ', translation:'Za'},
+    {character: 'じ', translation:'Zi'},
+    {character: 'ず', translation:'Zu'},
+    {character: 'ぜ', translation:'Ze'},
+    {character: 'ぞ', translation:'Zo'},
+    {character: 'だ', translation:'Da'},
+    {character: 'ぢ', translation:'Di'},
+    {character: 'づ', translation:'Du'},
+    {character: 'で', translation:'De'},
+    {character: 'ど', translation:'Do'},
+    {character: 'ば', translation:'Ba'},
+    {character: 'び', translation:'Bi'},
+    {character: 'ぶ', translation:'Bu'},
+    {character: 'べ', translation:'Be'},
+    {character: 'ぼ', translation:'Bo'},
+    {character: 'ぱ', translation:'Pa'},
+    {character: 'ぴ', translation:'Pi'},
+    {character: 'ぷ', translation:'Pu'},
+    {character: 'ぺ', translation:'Pe'},
+    {character: 'ぽ', translation:'Po'},
+  ];
 
+  katakanaAlphabet = [
+    {character: 'ア', translation:'A'},
+    {character: 'イ', translation:'I'},
+    {character: 'ウ', translation:'U'},
+    {character: 'エ', translation:'E'},
+  ];
+
+  kanjiAlphabet = [
+    {character: '一', translation:'one'},
+    {character: '二', translation:'two'},
+    {character: '三', translation:'three'},
+    {character: '四', translation:'four'},
+    {character: '五', translation:'five'},
+    {character: '六', translation:'six'},
+    {character: '七', translation:'seven'},
+    {character: '八', translation:'eight'},
+    {character: '九', translation:'nine'},
+    {character: '十', translation:'ten'},
+  ];
   constructor() {
    }
 
@@ -24,7 +117,7 @@ export class ProgressBlockComponent implements OnInit {
 
   // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
   ngAfterContentInit(): void {
-    this.setAlphabet(this.letter);
+    this.setAlphabet(this.letter, this.alphabetType);
   }
   // TODO: dynamically set the progress percentage, #69, Maryam Mohamad Al Mahdi
   setStyleCalc(){
@@ -32,26 +125,43 @@ export class ProgressBlockComponent implements OnInit {
     return this.styles;
   }
   // TODO: dynamically set the letter, #69, Maryam Mohamad Al Mahdi
-  setAlphabet(letter: number){
-    if(letter === 1){
-      this.japaneseLetter = this.alphabet[0];
-      this.translation = 'A';
+  setAlphabet(letter: string, alphabetType: string){
+    if(alphabetType === 'hiragana'){
+
+    this.hiraganaAlphabet.forEach(element => {
+
+      if(element.translation === letter)
+      {
+        this.japaneseLetter= element.character;
+        this.translation = element.translation;
+      }
+
+    });
+
     }
-    if(letter === 2){
-      this.japaneseLetter = this.alphabet[1];
-      this.translation = 'I';
+    if(alphabetType === 'katakana'){
+
+      this.katakanaAlphabet.forEach(element => {
+
+        if(element.translation === letter)
+        {
+          this.japaneseLetter= element.character;
+          this.translation = element.translation;
+        }
+
+      });
+
     }
-    if(letter === 3){
-      this.japaneseLetter = this.alphabet[2];
-      this.translation = 'U';
-    }
-    if(letter === 4){
-      this.japaneseLetter = this.alphabet[3];
-      this.translation = 'E';
-    }
-    if(letter === 5){
-      this.japaneseLetter = this.alphabet[4];
-      this.translation = 'O';
+    if(alphabetType === 'kanji'){
+      this.kanjiAlphabet.forEach(element => {
+
+        if(element.translation === letter)
+        {
+          this.japaneseLetter= element.character;
+          this.translation = element.translation;
+        }
+
+      });
     }
   }
 
--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -22,22 +22,114 @@ export class ProgressPage implements OnInit {
 
   htmlToAdd = '';
 
+  hiragana = 'hiragana';
+  katakana = 'katakana';
+  kanji = 'kanji';
+
+  map = new Map();
+
   constructor(private router: Router) { }
 
   ngOnInit() {
     this.char = localStorage.getItem('char');
     this.percent = +localStorage.getItem('percentage');
     // eslint-disable-next-line max-len
+    this.map.set('A', 'A');
+    this.map.set('I','I');
+    this.map.set('U','U');
+    this.map.set('E','E');
+    this.map.set('O','O');
+    this.map.set('Ka','Ka');
+    this.map.set('Ki','Ki');
+    this.map.set('Ku','Ku');
+    this.map.set('Ke','Ke');
+    this.map.set('Ko','Ko');
+    this.map.set('Sa','Sa');
+    this.map.set('Si','Si');
+    this.map.set('Su','Su');
+    this.map.set('Se','Se');
+    this.map.set('So','So');
+    this.map.set('Ta','Ta');
+    this.map.set('Ti','Ti');
+    this.map.set('Tu','Tu');
+    this.map.set('Te','Te');
+    this.map.set('To','To');
+    this.map.set('Na','Na');
+    this.map.set('Ni','Ni');
+    this.map.set('Nu','Nu');
+    this.map.set('Ne','Ne');
+    this.map.set('No','No');
+    this.map.set('Ha','Ha');
+    this.map.set('Hi','Hi');
+    this.map.set('Hu','Hu');
+    this.map.set('He','He');
+    this.map.set('Ho','Ho');
+    this.map.set('Ma','Ma');
+    this.map.set('Mi','Mi');
+    this.map.set('Mu','Mu');
+    this.map.set('Me','Me');
+    this.map.set('Mo','Mo');
+    this.map.set('Ya','Ya');
+    this.map.set('Yu','Yu');
+    this.map.set('Yo','Yo');
+    this.map.set('Ra','Ra');
+    this.map.set('Ri','Ri');
+    this.map.set('Ru','Ru');
+    this.map.set('Re','Re');
+    this.map.set('Ro','Ro');
+    this.map.set('Wa','Wa');
+    this.map.set('Wi','Wi');
+    this.map.set('We','We');
+    this.map.set('Wo','Wo');
+    this.map.set('Ga','Ga');
+    this.map.set('Gi','Gi');
+    this.map.set('Gu','Gu');
+    this.map.set('Ge','Ge');
+    this.map.set('Go','Go');
+    this.map.set('Za','Za');
+    this.map.set('Zi','Zi');
+    this.map.set('Zu','Zu');
+    this.map.set('Ze', 'Ze');
+    this.map.set('Zo','Zo');
+    this.map.set('Da','Da');
+    this.map.set('Di','Di');
+    this.map.set('Du','Du');
+    this.map.set('De','De');
+    this.map.set('Do','Do');
+    this.map.set('Ba','Ba');
+    this.map.set('Bi','Bi');
+    this.map.set('Bu','Bu');
+    this.map.set('Be','Be');
+    this.map.set('Bo','Bo');
+    this.map.set('Pa','Pa');
+    this.map.set('Pi','Pi');
+    this.map.set('Pu','Pu');
+    this.map.set('Pe','Pe');
+    this.map.set('Po','Po');
+    this.map.set('one','one');
+    this.map.set('two','two');
+    this.map.set('three','three');
+    this.map.set('four','four');
+    this.map.set('five','five');
+    this.map.set('six','six');
+    this.map.set('seven','seven');
+    this.map.set('eight','eight');
+    this.map.set('nine','nine');
+    this.map.set('ten','ten');
   }
-  // TODO: set the character and percentage, #73, Maryam Mohamad Al Mahdi
+  // TODO: set the character and percentage; #73; Maryam Mohamad Al Mahdi
   setDisplay(char: string, percent: number)
   {
     this.char = char;
     this.percent = percent;
   }
 
-  // TODO: navigates to home page, #73, Maryam Mohamad Al Mahdi
+  // TODO: navigates to home page; #73; Maryam Mohamad Al Mahdi
   setHome(){
     this.router.navigate(['/home']);
+  }
+
+  getLetter(letter: string){
+    return this.map.get(letter);
   }
 }
