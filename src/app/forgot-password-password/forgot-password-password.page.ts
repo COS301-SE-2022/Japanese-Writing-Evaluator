ac55be9404b73044c4f3196bbcb311004ff52347
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-07-13 23:46:48 +0200

    ✨ Added html of forgot password password

--- src/app/forgot-password-password/forgot-password-password.page.ts
+++ src/app/forgot-password-password/forgot-password-password.page.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPasswordPage implements OnInit {
 
-  constructor() { }
+  constructor(){}
 
   ngOnInit() {
+  }
+
+  onSubmitPassword(){
   }
 
 }
