2b7daa5e08e0fbbfb9f4ebb7ba9376c5ab8b52a7
Vhulenda88 <u19050993@tuks.co.za>
2022-07-15 12:40:09 +0200

    fixed sonarcloud bugs

--- src/app/Interceptor/loader.interceptor.ts
+++ src/app/Interceptor/loader.interceptor.ts
@@ -2,10 +2,8 @@ import { Injectable } from '@angular/core';
 import {  HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { EMPTY, Observable } from 'rxjs';
 import { LoadingController } from '@ionic/angular';
-//import {AnimationBuilder} from '@angular/animations';
 import { catchError, delay, map, retryWhen } from 'rxjs/operators';
 import { UploadPage } from '../upload/upload.page';
-//import { AppServiceService } from '../services/app-service.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor{
@@ -15,121 +13,138 @@ export class LoadingInterceptor implements HttpInterceptor{
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-        //intercepts an http request
         if(req.url.endsWith('login')){
-            return next.handle(req).pipe(
-                catchError(err => {
-                    console.log('login error' + err);
-                    //show that there is an error in the upload page
-                    return EMPTY;
-                }),
-                retryWhen(err => {
-                    let retryRequestCount = 1;// remove later
-                    return err.pipe(
-                        delay(2000),
-                        map(error => {
-                            if(retryRequestCount === 2){
-                                throw error;
-                            }
-                            else{
-                                retryRequestCount++;
-                            }
-                            return error;
-                        })
-                    );
-                })
-            );
+            return this.loginIntercept(req,next);
         }
 
         if(req.url.endsWith('register')){
-            return next.handle(req).pipe(
-                catchError(err => {
-                    console.log('register error' + err);
-                    //show that there is an error in the upload page
-                    return EMPTY;
-                }),
-                retryWhen(err => {
-                    let retryRequestCount = 1;// remove later
-                    return err.pipe(
-                        delay(2000),
-                        map(error => {
-                            if(retryRequestCount === 2){
-                                throw error;
-                            }
-                            else{
-                                retryRequestCount++;
-                            }
-                            return error;
-                        })
-                    );
-                })
-            );
+            return this.registerIntercept(req,next);
         }
 
         if(req.url.endsWith('upload')){
-            const msg: string = '<link href="https://fonts.googleapis.com/css?family=ZCOOL XiaoWei" rel="stylesheet">'+
-            '<ion-content>'+
-              '<div class="infinity-loader">'+
-                '<div class="bg">'+
-                 '   <div class="left-bg"></div>'+
-                  '  <div class="right-bg"></div>'+
-                '</div>'+
-                '<div class="fg"> <!--foreground circles-->'+
-                  '<div class="top-left-rect">'+
-                    '<div></div>'+
-                  '</div>'+
-                  '<div class="bottom-right-rect">'+
-                    '<div></div>'+
-                  '</div>'+
-                  '<div class="top-right-rect">'+
-                    '<div></div>'+
-                  '</div>'+
-                  '<div class="bottom-left-rect">'+
-                    '<div></div>'+
-                  '</div>'+
-                '</div>'+
-              '</div>'+
-              '<p class="textOne"><b>LOADING...</b></p>'+
-            '</ion-content>';
-            this.loadingController.getTop().then( isloading => {
-                if (!isloading) {
-                    //let animeBuilder: AnimationBuilder = new AnimationBuilder();
-                    this.loadingController.create({
-                        spinner: null,
-                        message: msg,
-                        animated: true,
-                        cssClass: 'loader'
-                        // enterAnimation: this.animeBuilder.build ,
-                        // leaveAnimation: animeBuilder
-                    }).then(loader => loader.present());
-                }
-            });
-
-            return next.handle(req).pipe(
-                catchError(err => {
-                    //console.log('error' + err);
-                    //show that there is an error in the upload page
-                    this.loadingController.dismiss();
-                    this.uploadPage.showScore(-1);
-                    return EMPTY;
-                }),
-                retryWhen(err => {
-                    let retryRequestCount = 1;// remove later
-                    return err.pipe(
-                        delay(2000),
-                        map(error => {
-                            if(retryRequestCount === 2){
-                                throw error;
-                            }
-                            else{
-                                retryRequestCount++;
-                            }
-                            return error;
-                        })
-                    );
-                })
-            );
+            return this.uploadIntercept(req,next);
         }
     }
+
+    loginIntercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        //intercepts an http request
+        return next.handle(req).pipe(
+            catchError(err => {
+                console.log('login error' + err);
+                //show that there is an error in the upload page
+                return EMPTY;
+            }),
+            retryWhen(err => {
+                let retryRequestCount = 1;// remove later
+                return err.pipe(
+                    delay(2000),
+                    map(error => {
+                        if(retryRequestCount === 2){
+                            throw error;
+                        }
+                        else{
+                            retryRequestCount++;
+                        }
+                        return error;
+                    })
+                );
+            })
+        );
+    }
+
+    registerIntercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        return next.handle(req).pipe(
+            catchError(err => {
+                console.log('register error' + err);
+                //show that there is an error in the upload page
+                return EMPTY;
+            }),
+            retryWhen(err => {
+                let retryRequestCount = 1;// remove later
+                return err.pipe(
+                    delay(2000),
+                    map(error => {
+                        if(retryRequestCount === 2){
+                            throw error;
+                        }
+                        else{
+                            retryRequestCount++;
+                        }
+                        return error;
+                    })
+                );
+            })
+        );
+    }
+
+    uploadIntercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const msg: string = '<link href="https://fonts.googleapis.com/css?family=ZCOOL XiaoWei" rel="stylesheet">'+
+        '<ion-content>'+
+            '<div class="infinity-loader">'+
+            '<div class="bg">'+
+                '   <div class="left-bg"></div>'+
+                '  <div class="right-bg"></div>'+
+            '</div>'+
+            '<div class="fg"> <!--foreground circles-->'+
+                '<div class="top-left-rect">'+
+                '<div></div>'+
+                '</div>'+
+                '<div class="bottom-right-rect">'+
+                '<div></div>'+
+                '</div>'+
+                '<div class="top-right-rect">'+
+                '<div></div>'+
+                '</div>'+
+                '<div class="bottom-left-rect">'+
+                '<div></div>'+
+                '</div>'+
+            '</div>'+
+            '</div>'+
+            '<p class="textOne"><b>LOADING...</b></p>'+
+        '</ion-content>';
+        this.loadingController.getTop().then( isloading => {
+            if (!isloading) {
+                this.loadingController.create({
+                    spinner: null,
+                    message: msg,
+                    animated: true,
+                    cssClass: 'loader'
+                    // enterAnimation: this.animeBuilder.build ,
+                    // leaveAnimation: animeBuilder
+                }).then(loader => loader.present());
+            }
+        });
+
+        return next.handle(req).pipe(
+            catchError(() => {
+                //console.log('error' + err);
+                //show that there is an error in the upload page
+                this.loadingController.dismiss();
+                this.uploadPage.showScore(-1);
+                return EMPTY;
+            }),
+            retryWhen(err => {
+                let retryRequestCount = 1;// remove later
+                return err.pipe(
+                    delay(2000),
+                    map(error => {
+                        if(retryRequestCount === 2){
+                            throw error;
+                        }
+                        else{
+                            retryRequestCount++;
+                        }
+                        return error;
+                    })
+                );
+            })
+        );
+    }
+
 }
+
+
+
