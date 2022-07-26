558f6308e2d852f156d8fbef28dabaa63afb9ed0
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-07-26 17:46:46 +0200

    ✨ Adapted home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,17 +8,10 @@ import character_sets from '../shared/character_data/character_sets.json';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  //images: CharacterStyle[]; // listy of images from firebase
-  groups: string[] =  ['vowels','k','t'];
-  style: string;
-  headingOne: string;
-
   currentAlphabet: any;
 
 
   constructor(private service: AppServiceService, private router: Router) {
-    this.style = 'Hiragana';
-    this.headingOne = 'Vowels';
 
     this.currentAlphabet = character_sets.hiragana;//this.hiraganaAlphabet;
     console.log();
@@ -50,42 +43,17 @@ export class HomePage implements OnInit {
 
   }
 
-  // TODO: the page components are dynamically updated based on the writing style selected by the used , #73, Maryam Mohamad Al Mahdi
-  writingStyle(style) {
-    this.style = style;
-
-    if(style === 'Hiragana'){
-      this.navigateHome();}
-
-    if(style === 'Katakana'){
-      this.navigateKatakana();}
-
-    if(style === 'Kanji'){
-      this.navigateKanji();}
-
-
-  }
-
   // TODO: components are dynamically updated for katakana, #73, Maryam Mohamad Al Mahdi
   navigateKatakana(){
-    this.headingOne = 'Vowels';
-
-    this.currentAlphabet = character_sets.katakana;
 
   }
 
   // TODO: components are dynamically updated for hiragana, #73, Maryam Mohamad Al Mahdi
   navigateHome(){
-    this.headingOne = 'Vowels';
-
-    this.currentAlphabet = character_sets.hiragana;
   }
 
   // TODO: components are dynamically updated for kanji, #73, Maryam Mohamad Al Mahdi
   navigateKanji(){
-    this.headingOne = 'Numbers';
-
-    this.currentAlphabet = character_sets.kanji;
   }
 
 }
