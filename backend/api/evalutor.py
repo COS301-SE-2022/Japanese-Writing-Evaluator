d68cf039da71aa2fa973291c0dc2afd1309360a2
Siphesihle05 <u19078286@tuks.co.za>
2022-07-26 18:15:51 +0200

    substituted old model with new one

--- backend/api/evalutor.py
+++ backend/api/evalutor.py
@@ -6,7 +6,6 @@ import base64
 
 class Evaluator(object):
     def __init__(self, file, input_char):
-        self.file = file
         self.char = input_char
         self.predition = -1
         self.dataset = ['a','i', 'u', 'e', 'o','ka','ki','ku','ke','ko','sa','shi','su','se','so','ta','chi','tsu','te','to','na','ni','nu','ne','no','ha','hi','fu','he','ho','ma','mi','mu','me','mo','ya','yu','yo','ra','ri','ru','re','ro','wa','wo','wi' ,'we','n']
@@ -42,25 +41,11 @@ class Evaluator(object):
             return None
         
     def mockTestCharacter(self):
-        model = tf.keras.models.load_model('../ai/models/characterRec.h5')
-        pre = model.predict([self.prepare()]).flatten()
+        pre = self.model.predict([self.prepare()]).flatten()
         pre = tf.nn.sigmoid(pre)
 
         pre = tf.where(pre < 0.5, 0, 1)
         print('\nprediction:\n', self.dataset[pre.numpy()[0]])
-
-    def testImage(self):
-        self.testCharacter()
-        if(self.char == 'A'):
-            model = tf.keras.models.load_model('backend/ai/modelA.h5')
-            self.prediction = float(model.predict([self.prepare()])[0][0])
-            return self.prediction
-        elif(self.char == 'U'):
-            model = tf.keras.models.load_model('backend/ai/modelU.h5')
-            self.prediction = float(model.predict([self.prepare()])[0][0])
-            print('\n In Model U and score: ' + str(self.prediction))
-            return self.prediction
-
 
 if __name__ == '__main__':
     # e = Evaluator('predict_data/false.png', '*')
