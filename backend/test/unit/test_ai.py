d9b5b2eff04ee83849286ed5ca29b8e7d2a4c905
Siphesihle05 <u19078286@tuks.co.za>
2022-08-02 18:54:47 +0200

    Update test_ai.py

--- backend/test/unit/test_ai.py
+++ backend/test/unit/test_ai.py
@@ -1,35 +1,36 @@
 import sys
 from PIL import Image
+import warnings
 
-# sys.path.append('backend/api/')
+sys.path.append('backend/api/')
 sys.path.append('api')
 sys.path.append('ai')
 from backend.api.evaluator import Evaluator
 
 def test_model_Hiragana_Evaluate_Characte():
     i = Image.open('backend/test/unit/a.jpg')
-    i.save('../../api/imageToSave.png')
+    i.save('backend/api/imageToSave.png')
     e = Evaluator('hiragana', '')
     result = e.testCharacter()
     assert result != None
     
 def test_model_Kanji_Evaluate_Character():
-    i = Image.open('backend/test/unit/kanji.jpg')
-    i.save('../../api/imageToSave.png')
+    i = Image.open('backend/test/unit/kanji.png')
+    i.save('backend/api/imageToSave.png')
     e = Evaluator('kanji', '')
     result = e.testCharacter()
     assert result != None
         
 def test_model_Hiragana_Evaluate_False_Image():
     i = Image.open('backend/test/unit/error.png')
-    i.save('../../api/imageToSave.png')
+    i.save('backend/api/imageToSave.png')
     e = Evaluator('hiragana', '')
     result = e.testCharacter()
     assert result != None
     
 def test_model_Kanji_Evaluate_False_Image():
     i = Image.open('backend/test/unit/error.png')
-    i.save('../../api/imageToSave.png')
+    i.save('backend/api/imageToSave.png')
     e = Evaluator('kanji', '')
     result = e.testCharacter()
     assert result != None
