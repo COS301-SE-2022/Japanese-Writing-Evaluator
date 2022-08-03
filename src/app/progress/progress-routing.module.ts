f819ae6ddd1a6b8c4c0f4d5b68e57f77a6b1ff34
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-08-03 12:17:56 +0200

    ✨ Created a page for modal

--- src/app/progress/progress-routing.module.ts
+++ src/app/progress/progress-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
   {
     path: '',
     component: ProgressPage
+  },  {
+    path: 'modal',
+    loadChildren: () => import('./modal/modal.module').then( m => m.ModalPageModule)
   }
+
 ];
 
 @NgModule({
