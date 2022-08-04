8655f545122627b2693996450d9bdaaeb1b9798c
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-08-04 20:28:30 +0200

    ✨ Added chart to modal

--- src/app/progress/graph-modal/graph-modal.page.ts
+++ src/app/progress/graph-modal/graph-modal.page.ts
@@ -1,17 +1,61 @@
-import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AfterViewInit, ElementRef, ViewChild , Component} from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-graph-modal',
   templateUrl: './graph-modal.page.html',
   styleUrls: ['./graph-modal.page.scss'],
 })
-export class GraphModalPage implements OnInit {
+export class GraphModalPage implements AfterViewInit {
+  @ViewChild('barCanvas') private barCanvas: ElementRef;
+
+  barChart: any;
 
   constructor(public modalController: ModalController) { }
-
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.barChartMethod();
   }
+
+  barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [200, 50, 30, 15, 20, 34],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
 
   close() {
     this.modalController.dismiss();
