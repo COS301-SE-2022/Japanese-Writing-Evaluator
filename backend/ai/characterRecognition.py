ab2e1f923ea1df725177fb7e9132222491217c8c
Siphesihle05 <u19078286@tuks.co.za>
2022-08-16 20:42:29 +0200

    Update characterRecognition.py

--- backend/ai/characterRecognition.py
+++ backend/ai/characterRecognition.py
@@ -11,16 +11,17 @@ from random import shuffle
 from PIL import Image
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-import pandas as pd
+import tensorflow_hub as hub
 from keras.losses import categorical_crossentropy
 from keras.metrics import categorical_accuracy
 from datetime import datetime
 import json
 
 class CharacterRecognition():
-    def __init__(self, name):
+    def __init__(self, name, num):
         self.version = name
         self.e = 10
+        self.num_classes = num
     """
         createDataset: Creates the training, test and validation dataset.
         parameters:
@@ -37,8 +38,8 @@ class CharacterRecognition():
         val = os.path.join(path, 'validation')
         self.img_size = (150, 150)
         
-        train_data = tf.keras.utils.image_dataset_from_directory(train, shuffle = True, batch_size = train_size, image_size = img_size)
-        val_data = tf.keras.utils.image_dataset_from_directory(val, shuffle = True, batch_size = val_size, image_size = img_size)
+        train_data = tf.keras.utils.image_dataset_from_directory(train, shuffle = True, batch_size = train_size, image_size = self.img_size)
+        val_data = tf.keras.utils.image_dataset_from_directory(val, shuffle = True, batch_size = val_size, image_size = self.img_size)
 
         val_batches = tf.data.experimental.cardinality(val_data)
         test_data = val_data.take(val_batches // 5)
@@ -67,35 +68,19 @@ class CharacterRecognition():
     """  
     def createModel(self):  
         print('\nCreating the model......')  
-        sequentail_layer = tf.keras.Sequential([
-            tf.keras.layers.RandomFlip('horizontal'),
-            tf.keras.layers.RandomRotation(0.3),
-        ])
-        
         #create base model
-        base_model = tf.keras.application.ResNet50V2(input_shape  = self.img_size, include_top = False, weight='imagenet')
-        
-        #freeze the convolutional base
-        base_model.trainable = False
-        base_model.summary()
-        image_batch, label_batch = next(iter(self.train_data))
-        feature_batch = base_model(image_batch)
-        
-        #Adding a classification head
-        global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-        feature_batch_average = global_average_layer(feature_batch)
-        
-        #Apply the dense layer
-        prediction_layer = tf.keras.layers.layers.Dense(1, activation = 'softmax')
-        
-        input = tf.keras.Input(shape = self.img_size + (3))
-        x = sequentail_layer(input)
-        x = base_model(x, training=False)
-        x = global_average_layer(x)
-        x = tf.keras.layers.Dropout(0.2)(x)
-        outputs = prediction_layer(x)
-        self.model = tf.keras.Model(input, outputs)
-        
+        base_model = tf.keras.application.ResNet50V2(input_shape = self.img_size, include_top = False, weight='imagenet')
+        feature_layer = hub.KerasLayer(
+            base_model,
+            input_shape=(150,150,3),
+            trainable=False
+        )
+        self.model = tf.keras.Sequential([
+            feature_layer,
+            tf.keras.layers.Dense(self.num_classes, activation = "softmax")
+        ])
+        self.model.summary()
+              
         #compile the model
         base_learning_rate = 0.0001
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
