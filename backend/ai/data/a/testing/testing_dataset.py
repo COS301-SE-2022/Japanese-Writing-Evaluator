2d6421ee71b79bfeca95d28679eda7a9350cea4d
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-16 19:11:27 +0200

    Update testing_dataset.py

--- backend/ai/data/a/testing/testing_dataset.py
+++ backend/ai/data/a/testing/testing_dataset.py
@@ -16,7 +16,7 @@ class testing_dataset:
 
     #create the folders for the testing dataset
     #rotate_15
-    def rotate_image(self,angle):
+    def rotate_image_(self,angle):
         self.count = self.count + 1
         for i in range (0,3):
             manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
@@ -26,12 +26,24 @@ class testing_dataset:
                 self.count = self.count + 1
 
 
+
         
 if __name__ == '__main__':
     testing_dataset = testing_dataset()
     #testing_dataset.rotate_image()
     counter = testing_dataset.count
+    #rotate_15
     testing_dataset.rotate_image(15)
+    #rotate_30
+    testing_dataset.rotate_image(30)
+    #rotate_45
+    testing_dataset.rotate_image(45)
+    #rotate_-15
+    testing_dataset.rotate_image(-15)
+    #rotate_-30
+    testing_dataset.rotate_image(-30)
+    #rotate_-45
+    testing_dataset.rotate_image(-45)
     print(counter)
     print("done")
 
