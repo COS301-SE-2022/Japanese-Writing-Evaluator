bd7f44b42cd93916d82553d05a02526fa6487cff
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-16 12:54:00 +0200

    images_a

--- backend/ai/data/a/testing/testing_dataset.py
+++ backend/ai/data/a/testing/testing_dataset.py
@@ -10,3 +10,12 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot
+count = 0 
+#rotate_15
+def rotate_image(image, angle):
+    for i in range (0,3):
+        manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
+        manipulated = manipulated.rotate(15)
+        for j in range (0,10):
+            manipulated.save('picture_'+str(count) + '.png')
+            count = count + 1
