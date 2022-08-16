61c6ccbc7605850a3c27c6dcf283ee925a0704b0
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-16 12:44:27 +0200

    ⚡️

--- backend/ai/data/a/testing/testing_dataset.py
+++ backend/ai/data/a/testing/testing_dataset.py
@@ -5,3 +5,8 @@ import numpy as np
 from random import seed
 from random import randint
 import matplotlib.pyplot as plt
+from numpy import expand_dims
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
