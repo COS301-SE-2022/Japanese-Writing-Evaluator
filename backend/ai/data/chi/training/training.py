19a96cf64c8cf0a25d44fd2c0a3de6bcc3a9ae12
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 18:43:24 +0200

    Update training.py

--- backend/ai/data/chi/training/training.py
+++ backend/ai/data/chi/training/training.py
@@ -50,3 +50,12 @@ class training:
                 manipulated.save('./training/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
+    """
+   zoom_image:
+        takes the different images in pictures folder and zooms in on them 
+    request body: 
+       
+        counter: the counter of how many images are in the training folder
+    return:
+        counter
+"""     
