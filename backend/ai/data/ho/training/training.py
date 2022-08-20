986572741b65633494ac91cfddd4e568fbac97e5
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 20:23:57 +0200

    Create training.py

---
+++ backend/ai/data/ho/training/training.py
@@ -0,0 +1,34 @@
+import io
+from PIL import Image , ImageFilter 
+import tensorflow as tf
+import numpy as np
+from random import seed
+from random import randint
+import matplotlib.pyplot as plt
+from numpy import expand_dims
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+import cv2
+class training: 
+
+    """
+    blur function:
+        takes the different images in pictures folder and blurs them
+    request body: 
+        blurr: the degree for GaussianBlur
+        counter: the counter of how many images are in the training folder
+    return:
+        counter
+""" 
+    def blur(self,blurr,counter): 
+        #blur
+        for i in range (0,38):
+            manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
+            
+            gaussImage = manipulated.filter(ImageFilter.GaussianBlur(blurr))
+            for j in range (0,100):
+                gaussImage.save('./training/picture_'+str(counter) + '.png')
+                counter = counter + 1
+        return counter
