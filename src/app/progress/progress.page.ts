22ccecffff6c77a2f7850225c20e9b04223bb932
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-08-20 21:53:34 +0200

    ✨ Adjust progress

--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -21,9 +21,9 @@ export class ProgressPage implements OnInit {
 
   //Data for progress
   progressArray: {writingStyle: string; url: string; character: string; score: string; uploadDate: string}[];
-  scoresChar: { char: string; score: string; date: string }[];
   object: { char: string; score: string; date: string };
   progressSummary =  new Map<string, {score: string; date: string }[]> ();
+  writingStylesArray: string[];
 
   char = '';
   percent = 0;
@@ -49,6 +49,10 @@ export class ProgressPage implements OnInit {
 
     //testPurposes
     //also note the naming conventions are incorrect from the API so they need be changed
+    this.writingStylesArray = [
+    'hiragana', 'katakana', 'kanji'
+    ];
+
     this.progressArray = [
       { writingStyle: 'hiragana', url: ' ', character: 'A', score: '25', uploadDate: '2022-07-19'  },
       { writingStyle: 'hiragana', url: ' ', character: 'A', score: '50', uploadDate: '2022-07-20'  },
@@ -60,11 +64,14 @@ export class ProgressPage implements OnInit {
       { writingStyle: 'hiragana', url: ' ', character: 'Ha', score: '84', uploadDate: '2022-08-15'  },
       { writingStyle: 'kanji', url: ' ', character: 'two', score: '36', uploadDate: '2022-08-22'  },
       { writingStyle: 'kanji', url: ' ', character: 'two', score: '80', uploadDate: '2022-08-23'  },
+      { writingStyle: 'kanji', url: ' ', character: 'one', score: '80', uploadDate: '2022-08-23'  },
+      { writingStyle: 'kanji', url: ' ', character: 'three', score: '80', uploadDate: '2022-08-23'  },
       { writingStyle: 'katakana', url: ' ', character: 'A', score: '98', uploadDate: '2022-08-30'  },
       { writingStyle: 'hiragana', url: ' ', character: 'A', score: '10', uploadDate: '2022-08-30'  },
       { writingStyle: 'katakana', url: ' ', character: 'A', score: '10', uploadDate: '2022-08-30'  },
       { writingStyle: 'katakana', url: ' ', character: 'A', score: '88', uploadDate: '2022-09-15'  },
       { writingStyle: 'katakana', url: ' ', character: 'A', score: '70', uploadDate: '2022-09-18'  },
+      { writingStyle: 'katakana', url: ' ', character: 'U', score: '60', uploadDate: '2022-09-18'  },
     ];
 
 
@@ -80,6 +87,11 @@ export class ProgressPage implements OnInit {
       let keyString = '';
       keyString += this.progressArray[i].character + '_';
       keyString += this.progressArray[i].writingStyle;
+
+      if(true)
+      {
+      //   //this.writingStylesArray.push(this.progressArray[i].writingStyle);
+      }
 
       if(this.progressSummary.has(keyString)){
 
