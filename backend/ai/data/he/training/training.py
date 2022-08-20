615c6474389fcaed1db614d3f09a6ffcac6c6868
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 19:57:50 +0200

    Create training.py

---
+++ backend/ai/data/he/training/training.py
@@ -0,0 +1,13 @@
+import io
+from PIL import Image , ImageFilter 
+import tensorflow as tf
+import numpy as np
+from random import seed
+from random import randint
+import matplotlib.pyplot as plt
+from numpy import expand_dims
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+import cv2
