a0cf6b5806c197812cd2cb0f715bde2ded12260b
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 19:12:23 +0200

    Update training.py

--- backend/ai/data/e/training/training.py
+++ backend/ai/data/e/training/training.py
@@ -32,3 +32,13 @@ class training:
                 gaussImage.save('./training/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
+
+    """
+   rotate_image_:
+        takes the different images in pictures folder and roates them by a degree
+    request body: 
+        angle: the degree for the rotation
+        counter: the counter of how many images are in the training folder
+    return:
+        counter
+"""   
