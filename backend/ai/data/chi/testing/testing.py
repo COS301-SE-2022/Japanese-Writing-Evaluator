ef10ed9a11239a3ac187a5eda57c65122e172a49
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 18:28:34 +0200

    Update testing.py

--- backend/ai/data/chi/testing/testing.py
+++ backend/ai/data/chi/testing/testing.py
@@ -162,3 +162,11 @@ class testing:
                 manipulated.save('./testing/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
+
+if __name__ == '__main__':
+    testing = testing()
+  
+    counter = 0
+    print(counter)
+    counter= testing.rotate_image_(15,counter)
+    
