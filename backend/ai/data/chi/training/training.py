8aa0a797e498d74f86d2f7f2146e6e53fee3bdb2
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 18:52:21 +0200

    Update training.py

--- backend/ai/data/chi/training/training.py
+++ backend/ai/data/chi/training/training.py
@@ -153,4 +153,5 @@ if __name__ == '__main__':
     counter = training.rotate_image_(-30,counter)  
     counter =  training.rotate_image_(-45,counter)
     counter =  training.zoom_image(counter)
-    counter =  training.shear(counter)      
+    counter =  training.shear(counter)
+    counter =  training.transpose_90(counter)      
