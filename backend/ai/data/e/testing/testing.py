9a549b8927dd0f1b02f7cd1520d56138b041de9f
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-20 19:08:34 +0200

    Update testing.py

--- backend/ai/data/e/testing/testing.py
+++ backend/ai/data/e/testing/testing.py
@@ -152,3 +152,4 @@ if __name__ == '__main__':
     counter = testing.rotate_image_(-30,counter)
     counter =  testing.rotate_image_(-45,counter)
     counter =  testing.zoom_image(counter)
+    counter =  testing.shear(counter)
