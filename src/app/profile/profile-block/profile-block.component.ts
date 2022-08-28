e16c65c32f54198b0ae83ec003544e5c03e23c8b
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-08-28 10:07:51 +0200

    ✨ Created profile block component

---
+++ src/app/profile/profile-block/profile-block.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-block',
+  templateUrl: './profile-block.component.html',
+  styleUrls: ['./profile-block.component.scss'],
+})
+export class ProfileBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
