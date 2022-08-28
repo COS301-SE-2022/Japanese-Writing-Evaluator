9f1dc74c6366e863ac18a291fe4c5ffd99508a90
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-08-28 15:23:44 +0200

    ✨ Based of profile done

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { ProfileBlockComponent } from './profile-block/profile-block.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ProfilePage } from './profile.page';
     IonicModule,
     ProfilePageRoutingModule
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage, ProfileBlockComponent]
 })
 export class ProfilePageModule {}
