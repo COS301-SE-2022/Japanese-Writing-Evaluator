406325914e822f5c4df052d653945e7df977a367
Philippa29 <68788485+Philippa29@users.noreply.github.com>
2022-08-30 15:50:06 +0200

    Update testing.py

--- backend/ai/data/katakana/testing.py
+++ backend/ai/data/katakana/testing.py
@@ -23,3 +23,14 @@ class testing:
     return:
         counter
 """ 
+    def blur(self,blurr,counter): 
+        #blur
+        for i in range (0,38):
+            manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
+            
+            gaussImage = manipulated.filter(ImageFilter.GaussianBlur(blurr))
+            for j in range (0,5):
+                gaussImage.save('../../dataset/test/a/picture_'+str(counter) + '.png')
+                counter = counter + 1
+        return counter
+        
