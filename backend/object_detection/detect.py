30e4a500da7f1106ce040520d6622a7a6bdc9d05
Raymond0413 <sechaba836@gmail.com>
2022-09-12 14:31:24 +0200

    🐛 (backend-feature-object detection) add exception handling

--- backend/object_detection/detect.py
+++ backend/object_detection/detect.py
@@ -16,56 +16,60 @@ class detect:
             the objects detected in the image
     """
     def detect(image):
-        model = torch.hub.load('ultralytics/yolov5', 'yolov5x')
+        try:
+            model = torch.hub.load('ultralytics/yolov5', 'yolov5x')
 
-        img = image.partition(",")[2]
+            img = image.partition(",")[2]
 
-        with open("objectImage.jpeg", "wb") as fh:
-            fh.write(base64.b64decode(img))
+            with open("objectImage.jpeg", "wb") as fh:
+                fh.write(base64.b64decode(img))
 
-        im = "objectImage.jpeg"
+            im = "objectImage.jpeg"
 
-        results = model(im)
-        res = str(results.pandas().xyxy[0])
-        splitted = res.split('name')
+            results = model(im)
+            res = str(results.pandas().xyxy[0])
+            splitted = res.split('name')
+            
+            # print(results.print())
+
+            classes = re.findall(r'[a-zA-Z]+', splitted[1])
+            # print(classes)
+            # print(len(classes))
+
+            store = []
+            for i in classes:
+                if i in store: 
+                    continue
+                else:
+                    store.append(i)
+
+            translator = Translator()
+            convert = pykakasi.kakasi()
+            # store = translator.translate(text1, "ja", "en")
+
+            words = []
+            for i in store:
+
+                trans = translator.translate(i, "ja", "en")
+                translation = str(trans).split(",")
+                text = translation[2].split("text=")
+                characters = list(text[1])
+
+                word = text[1]
+                res = convert.convert(word)
+
+                for j in res:
         
-        # print(results.print())
+                    chars = list(j['hira'])
 
-        classes = re.findall(r'[a-zA-Z]+', splitted[1])
-        # print(classes)
-        # print(len(classes))
-
-        store = []
-        for i in classes:
-            if i in store: 
-                continue
-            else:
-                store.append(i)
-
-        translator = Translator()
-        convert = pykakasi.kakasi()
-        # store = translator.translate(text1, "ja", "en")
-
-        words = []
-        for i in store:
-
-            trans = translator.translate(i, "ja", "en")
-            translation = str(trans).split(",")
-            text = translation[2].split("text=")
-            characters = list(text[1])
-
-            word = text[1]
-            res = convert.convert(word)
-
-            for j in res:
-    
-                chars = list(j['hira'])
-
-                words.append({
-                    "Object": i,
-                    "Characters": chars,
-                    "Pronunciation": j['hepburn']
-                })
+                    words.append({
+                        "Object": i,
+                        "Characters": chars,
+                        "Pronunciation": j['hepburn']
+                    })
 
 
-        return jsonify({'response': words}), 200
+            return jsonify({'response': words}), 200
+        except Exception as e:
+            return jsonify({'response': e}), 400
+            
