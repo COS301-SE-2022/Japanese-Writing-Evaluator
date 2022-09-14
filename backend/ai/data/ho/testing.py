f3a6082ad4c21b662ce149ef81e1ba934b1e4d07
Siphesihle05 <u19078286@tuks.co.za>
2022-09-14 15:46:47 +0200

    Revert "Updated test data"
    This reverts commit 0b72eec1330ab9a6cbd91fd922b92664be0b8354.

--- backend/ai/data/ho/testing.py
+++ backend/ai/data/ho/testing.py
@@ -48,7 +48,7 @@ class testing:
         for i in range (0,38):
             manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
             manipulated = manipulated.rotate(angle)
-            for j in range (0,25):
+            for j in range (0,50):
                 manipulated.save('../../dataset/test/ho/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
@@ -75,7 +75,7 @@ class testing:
             batch = it.next()
             image = batch[0].astype('uint8')
             manipulated = Image.fromarray(image)
-            for j in range (0,25):
+            for j in range (0,50):
                 manipulated.save('../../dataset/test/ho/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
@@ -100,7 +100,7 @@ class testing:
             	[0, 0  , 1]])
             sheared_img = cv2.warpPerspective(img,M,(int(cols*1.2),int(rows*1.2)))
             plt.axis('off')
-            for j in range (0,25):
+            for j in range (0,50):
                 plt.imsave('../../dataset/test/ho/picture_'+str(counter) + '.png', sheared_img)
                 counter = counter + 1
         return counter
@@ -117,7 +117,7 @@ class testing:
         for i in range (0,38):
             manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
             manipulated = manipulated.transpose(Image.ROTATE_90)
-            for j in range (0,25):
+            for j in range (0,50):
                 manipulated.save('../../dataset/test/ho/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
@@ -134,7 +134,7 @@ class testing:
         for i in range (0,38):
             manipulated =Image.open('./pictures/test_'+ str(i) + '.png')
             manipulated = manipulated.transpose(Image.ROTATE_270)
-            for j in range (0,25):
+            for j in range (0,50):
                 manipulated.save('../../dataset/test/ho/picture_'+str(counter) + '.png')
                 counter = counter + 1
         return counter
