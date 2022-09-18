8271911fde6321366ba5840412a4eb9337236d81
Raymond0413 <sechaba836@gmail.com>
2022-09-18 20:59:09 +0200

    ✨ (backend-architecture-fixes) convert to flask app

--- backend/services/imageDB.py
+++ backend/services/imageDB.py
@@ -3,106 +3,134 @@ import json
 import os
 import pyrebase
 from flask import jsonify
+from flask import Flask, jsonify, request, session, redirect
+from flask_cors import CORS;
+import psycopg2
+import requests
 
-class imageDB:
-    def __init__(self,db):
-        self.db = db
-        
-    """
-    saveToDB function:
-        Saves users uploaded image information to database
-    parameters: 
-        id, file, imageChar, score, writingStyle
-    return:
-        response from db
-    """
-    def saveToDB(self, id, file, imageChar, score, writingStyle):
-        imagePath = "/users/"+str(id)+"/"+file
-        return self.db.saveImage(id, imagePath, imageChar, score, writingStyle)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+CORS(app)
 
-    """
-    getImages function:
-        gets all the users uploaded images
-    parameters: 
-        id
-    return:
-        response from db
-    """
-    def getImages(self, id):
-        return self.db.getImage(id)
+try:
+    conn2 = psycopg2.connect(host = os.getenv('Image_host'), database = os.getenv('Image_db'), user = os.getenv('Image_user'), password = os.getenv('Image_pass'))
+    curr2 = conn2.cursor()
+except Exception as e:
+    print("Could not connect to database", e)
+    
+"""
+saveToDB function:
+    Saves users uploaded image information to database
+parameters: 
+    id, file, imageChar, score, writingStyle
+return:
+    response from db
+"""
+def saveToDB(self, id, file, imageChar, score, writingStyle):
+    imagePath = "/users/"+str(id)+"/"+file
+    return self.db.saveImage(id, imagePath, imageChar, score, writingStyle)
 
-    """
-    getImagesUsers function:
-        gets all image information from database
-    parameters: 
-        none
-    return:
-        response from db
-    """
-    def getImageUsers(self):
-        return self.db.getImageUsers()
+"""
+getImages function:
+    gets all the users uploaded images
+parameters: 
+    id
+return:
+    response from db
+"""
+@app.route("/getImages", methods=["POST"])
+def getImages():
+    id = request.json["id"]
+    view_query = "SELECT * FROM image WHERE id=%s ORDER BY  upload_date DESC;"
+    curr2.execute(view_query, ([id]))
+    images = curr2.fetchall()
+    call = requests.post("http://127.0.0.1:5004/", images)
+    res = images.json()["response"]
+    return jsonify({'response': res}), 200    
 
-    def getUserAnalytics(self):
-        store = self.db.getImageUsers()
-        analytics = []
-        sum = 0
-        count = 0
-        
-        for i in store:
-            year = i[5].strftime("%Y-%m-%d").split('-')[0]
-            month = i[5].strftime("%Y-%m-%d").split('-')[1]
-            character = i[4].lower()
-            current = int(year) - 2022
+"""
+getImagesUsers function:
+    gets all image information from database
+parameters: 
+    none
+return:
+    response from db
+"""
+@app.route("/getImageUsers", methods=["GET"])
+def getImageUsers():
+    getUsers = "SELECT * FROM image";
+    curr2.execute(getUsers)
+    users = curr2.fetchall()
+    print(users)
+    res = []
+    for i in users:
+        res.append(i)
+    return jsonify({"response": res}), 200
 
-            if len(analytics) == 0 or current > len(analytics):
+def getUserAnalytics(self):
+    store = self.db.getImageUsers()
+    analytics = []
+    sum = 0
+    count = 0
+    
+    for i in store:
+        year = i[5].strftime("%Y-%m-%d").split('-')[0]
+        month = i[5].strftime("%Y-%m-%d").split('-')[1]
+        character = i[4].lower()
+        current = int(year) - 2022
 
-                for j in store:
-                    if j[4].lower() == character and year == j[5].strftime("%Y-%m-%d").split('-')[0] and month == j[5].strftime("%Y-%m-%d").split('-')[1]:
-                        sum += j[3]
-                        count += 1
-                analytics.append({
-                    year:{
-                        "year": year,
-                        month:{
-                            "month": month,
-                            character: {
-                                "writingStyle": character,
-                                "averageScore": sum/count}
-                        }
+        if len(analytics) == 0 or current > len(analytics):
+
+            for j in store:
+                if j[4].lower() == character and year == j[5].strftime("%Y-%m-%d").split('-')[0] and month == j[5].strftime("%Y-%m-%d").split('-')[1]:
+                    sum += j[3]
+                    count += 1
+            analytics.append({
+                year:{
+                    "year": year,
+                    month:{
+                        "month": month,
+                        character: {
+                            "writingStyle": character,
+                            "averageScore": sum/count}
                     }
-                })
-                sum = 0
-                count = 0
-
-            elif year in analytics[current] and month in analytics[current][str(year)] and character not in analytics[current][str(year)][str(month)]:
-                for j in store:
-                    if j[4].lower() == character and year == j[5].strftime("%Y-%m-%d").split('-')[0] and month == j[5].strftime("%Y-%m-%d").split('-')[1]:
-                        sum += j[3]
-                        count += 1
-                
-                z = {
-                    character: {
-                        "writingStyle": character,
-                        "averageScore": sum/count}
                 }
-                analytics[current][str(year)][str(month)].update(z)
-                sum = 0
-                count = 0
+            })
+            sum = 0
+            count = 0
+
+        elif year in analytics[current] and month in analytics[current][str(year)] and character not in analytics[current][str(year)][str(month)]:
+            for j in store:
+                if j[4].lower() == character and year == j[5].strftime("%Y-%m-%d").split('-')[0] and month == j[5].strftime("%Y-%m-%d").split('-')[1]:
+                    sum += j[3]
+                    count += 1
             
-            else:
-                for j in store:
-                    if j[4].lower() == character and year == str(j[5].strftime("%Y-%m-%d").split('-')[0]) and month == str(j[5].strftime("%Y-%m-%d").split('-')[1]):
-                        sum += j[3]
-                        count += 1
+            z = {
+                character: {
+                    "writingStyle": character,
+                    "averageScore": sum/count}
+            }
+            analytics[current][str(year)][str(month)].update(z)
+            sum = 0
+            count = 0
+        
+        else:
+            for j in store:
+                if j[4].lower() == character and year == str(j[5].strftime("%Y-%m-%d").split('-')[0]) and month == str(j[5].strftime("%Y-%m-%d").split('-')[1]):
+                    sum += j[3]
+                    count += 1
 
-                z = {month: {
-                    "month": month,
-                    character: { 
-                        "writingStyle": character,
-                        "averageScore": sum/count}}}
-                analytics[current][str(year)].update(z) 
-                sum = 0
-                count = 0
+            z = {month: {
+                "month": month,
+                character: { 
+                    "writingStyle": character,
+                    "averageScore": sum/count}}}
+            analytics[current][str(year)].update(z) 
+            sum = 0
+            count = 0
 
-        return jsonify({'response': analytics}), 200    
+    return jsonify({'response': analytics}), 200    
 
+if __name__ == '__main__':
+    # run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True, use_evalex=True)
+    app.run(debug = True, port = 5003)
