ab9e25a9c38b4c9d586d53fddc39371c96b93917
Raymond0413 <sechaba836@gmail.com>
2022-09-21 17:28:43 +0200

    update port and host

--- backend/api/app.py
+++ backend/api/app.py
@@ -369,4 +369,4 @@ def callObjectDetection():
 
 if __name__ == '__main__':
     # run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True, use_evalex=True)
-    app.run(debug = True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
