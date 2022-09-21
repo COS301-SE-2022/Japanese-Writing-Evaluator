447b89afdb23923617a47bf6bf83c0a95abd7a48
Raymond0413 <sechaba836@gmail.com>
2022-09-21 17:26:47 +0200

    add token

--- backend/services/authentication.py
+++ backend/services/authentication.py
@@ -1,3 +1,5 @@
+from functools import wraps
+import jwt
 import hashlib
 from ensurepip import version
 import json
@@ -21,6 +23,24 @@ try:
     curr = conn.cursor()
 except Exception as e:
     print("Could not connect to database", e)
+
+def token_required(function):
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        token = None
+        print(request.headers)
+        if 'user-token' in request.headers:
+            print("we have token")
+            token = request.headers['user-token']
+        if not token:
+            return jsonify({'response' : 'Token is missing !!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return jsonify({'response' : 'The token is invaild!'}), 401
+        return  function(*args, **kwargs)
+  
+    return decorated 
 
 """
     resetPassword function:
@@ -429,4 +449,4 @@ def getAllUsers():
     
 if __name__ == '__main__':
     # run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True, use_evalex=True)
-    app.run(debug = True, port = 5005)
+    app.run(port=int(os.environ.get("PORT", 5005)),host='0.0.0.0',debug=True)
