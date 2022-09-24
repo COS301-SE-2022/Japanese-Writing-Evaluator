f24a9487ba2cbec9cac547279ef806ef9c42cc18
Raymond0413 <sechaba836@gmail.com>
2022-09-24 13:59:43 +0200

    move detect.py

--- backend/services/detect/detect.py
+++ backend/services/detect/detect.py
@@ -10,9 +10,6 @@ from googletrans import Translator
 import pykakasi
 from flask import Flask, jsonify, request, session, redirect
 from flask_cors import CORS;
-import json
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from flask_wtf.csrf import CSRFProtect
 
 load_dotenv()
 
@@ -51,7 +48,9 @@ def token_required(function):
 def detect():
     try:
         model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-        img = str(request.json["image"]).partition(",")[2]
+
+        image = request.json["image"]
+        img = image.partition(",")[2]
 
         with open("objectImage.jpeg", "wb") as fh:
             fh.write(base64.b64decode(img))
@@ -95,15 +94,15 @@ def detect():
                 chars = list(j['hira'])
 
                 words.append({
-                    "Object": i,
-                    "Characters": chars,
-                    "Pronunciation": j['hepburn']
+                    "object": i,
+                    "characters": chars,
+                    "pronunciation": j['hepburn']
                 })
 
         return jsonify({'response': words}), 200
     except Exception as e:
-        return jsonify({'response': e}), 400
-            
+        return jsonify({'response': "please upload a valid image"}), 400
+        
 if __name__ == '__main__':
     # app.run(debug = True, host='0.0.0.0', port=5001)
     app.run(port=int(os.environ.get("PORT", 5001)),host='0.0.0.0',debug=True)
