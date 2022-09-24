f9d1f9734c59cd4e019b428041352ca2690bd51c
Vhulenda88 <u19050993@tuks.co.za>
2022-09-24 21:00:35 +0200

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.jwe',
   appName: 'japanese-writing-evaluator',
   webDir: 'www',
   bundledWebRuntime: false
