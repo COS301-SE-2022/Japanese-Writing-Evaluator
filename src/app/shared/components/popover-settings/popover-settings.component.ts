d81fc3227efa017460ec47bae8ae172ebdfedc29
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-09-25 15:53:30 +0200

    ✨ Added popover for delete and logout

---
+++ src/app/shared/components/popover-settings/popover-settings.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popover-settings',
+  templateUrl: './popover-settings.component.html',
+  styleUrls: ['./popover-settings.component.scss'],
+})
+export class PopoverSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
