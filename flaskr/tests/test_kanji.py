daba250023a85f7c6c73217057fef273f6d07c6c
Siphesihle05 <u19078286@tuks.co.za>
2022-09-26 23:21:25 +0200

    Create test_kanji.py

---
+++ flaskr/tests/test_kanji.py
@@ -0,0 +1,9 @@
+from flaskr import create_app
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+def testlistusers_invaild_user(client):
+    user = client.post("/kanji")
+    assert user.status_code == 401
