5deb3713d0ac661b3255f129db90b4c4ea400ed3
Maryam <100767058+Maryam-Al-Mahdi@users.noreply.github.com>
2022-09-28 15:24:50 +0200

    ✨ Testing

--- src/app/profile/popover/popover.component.spec.ts
+++ src/app/profile/popover/popover.component.spec.ts
@@ -1,15 +1,24 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+//import { PopoverController } from '@ionic/angular';
 import { PopoverComponent } from './popover.component';
 
 describe('PopoverComponent', () => {
   let component: PopoverComponent;
   let fixture: ComponentFixture<PopoverComponent>;
+  // const modalSpy = jasmine.createSpyObj('Modal', ['present']);
+  // const modalCtrlSpy = jasmine.createSpyObj('PopoverController', ['create']);
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PopoverComponent ],
+      providers: [
+        {
+          // provide: PopoverController,
+          // useValue: modalCtrlSpy
+        }
+      ],
+
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
@@ -20,5 +29,15 @@ describe('PopoverComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('when app initiates list is set', () => {
+    component.ngOnInit();
+    expect(component.list).toEqual(['edit privileges']);
+  });
+  it('does popover modal is presented once popoverOption is called', () => {
+    spyOn(component,'presentModal');
+    component.popoverOption('edit privileges');
+    expect(component.presentModal).toHaveBeenCalled();
+
   });
 });
