7bae1e680f90c13d3679238f2140735b6c3b33eb
Vhulenda88 <u19050993@tuks.co.za>
2022-09-29 10:13:50 +0200

    changes

--- src/app/services/appService/app-service.service.ts
+++ src/app/services/appService/app-service.service.ts
@@ -18,7 +18,8 @@ import { RoleInfo} from 'src/app/shared/interfaces/roleInfo';
 })
 export class AppServiceService {
 
-  baseURL = 'http://localhost:8080/';//localhost is 10.0.2.2 for android studios (change to localhost for website)
+  baseURL = 'https://jwe-api-gateway-cplmvcuylq-uc.a.run.app/';
+  //localhost is 10.0.2.2 for android studios http://localhost:8080/(change to localhost for website)
   //https://flask-api-1-cplmvcuylq-uc.a.run.app/
   private characterImage: CharacterImage = {
     characterName: ' ',
